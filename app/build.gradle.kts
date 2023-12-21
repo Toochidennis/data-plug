f711bd89a7d08fe4cc73546ec7b5a119d569a352
Toochidennis <dennistoochukwu@gmail.com>
2023-12-21 23:57:37 +0100

    Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.einfinitytechnology.dataplug"
+    namespace = "com.einfinitytechnology.einfinity"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.einfinitytechnology.dataplug"
+        applicationId = "com.einfinitytechnology.einfinity"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
@@ -39,10 +39,9 @@ android {
 dependencies {
 
     implementation ("androidx.core:core-splashscreen:1.0.1")
-    implementation ("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
